9054e6ed9ac648432655a0a2428771cb85386441
LluisArevalo <thorin119@gmail.com>
2016-11-29 12:50:48 +0100

    Stack solution

--- solution-code/src/DataStructures.js
+++ solution-code/src/DataStructures.js
@@ -1,8 +1,29 @@
-function DataStructures() {
+function StackDataStructure() {
+  this.stackControl = [];
+  this.MAX_SIZE = 8;
 }
 
-DataStructures.prototype.stack = function () {
+StackDataStructure.prototype.isEmpty = function () {
+  return this.stackControl.length === 0;
 };
 
-DataStructures.prototype.queue = function () {
+StackDataStructure.prototype.canPush = function () {
+  return this.stackControl.length < this.MAX_SIZE;
+};
+
+StackDataStructure.prototype.push = function (element) {
+  if (!this.canPush()) {
+    return "Stack Overflow";
+  }
+
+  this.stackControl.push (element);
+  return this.stackControl;
+};
+
+StackDataStructure.prototype.pop = function (element) {
+  if (this.isEmpty()) {
+    return "Stack Underflow";
+  }
+
+  return this.stackControl.pop();
 };
